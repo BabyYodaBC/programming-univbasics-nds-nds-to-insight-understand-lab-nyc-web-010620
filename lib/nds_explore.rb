ada92390c7e043f93b95a30f44556088106fdf5d
ricky wahba <rjwahba@gmail.com>
2020-01-03 20:39:45 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,5 +10,10 @@ end
 pretty_print_nds(directors_database)
 
 def print_first_directors_movie_titles
-  print pretty_print_nds(nds)
+  movies_ar = directors_database[0][:movies]
+  movies_ar_index = 0
+  while movies_ar_index < movies_ar.count do
+    puts movies_ar[movies_ar_index][:title]
+    movies_ar_index += 1 
+  end 
 end
